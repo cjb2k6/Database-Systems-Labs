6947d790bbbeb8692e942843940d873c8427dffb
cjb2k6 <cjb2k6@gmail.com>
2014-02-14 00:04:55 -0500

    Added first query

---
+++ lab4.sql
@@ -0,0 +1,15 @@
+﻿--Christopher Barnett
+--2/13/14
+--Lab 4
+
+--Query 1
+select distinct city
+from agents
+where aid in  (select aid
+		from orders
+		where cid in (select cid
+				from customers
+				where name = 'Basics')
+				)
+order by city asc;
+
