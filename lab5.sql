bee5cdf7181457dfc6e08d88d1511abe8714abd9
cjb2k6 <cjb2k6@gmail.com>
2014-02-21 00:30:08 -0500

    Added second query on lab 5

--- lab5.sql
+++ lab5.sql
@@ -10,3 +10,12 @@ inner join customers c
 	on c.cid = o.cid and c.name = 'Basics'
 order by city asc;
 
+--Query 2
+select distinct o2.pid 
+from orders o
+inner join customers c
+	on c.cid = o.cid
+inner join orders o2
+	on o2.aid = o.aid and c.city = 'Kyoto'
+order by pid asc;
+
