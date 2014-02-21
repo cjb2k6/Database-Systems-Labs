ffb6a5d641df22af5922cfdab338df35c2a8d288
cjb2k6 <cjb2k6@gmail.com>
2014-02-21 00:00:22 -0500

    Added first query on lab 5

---
+++ lab5.sql
@@ -0,0 +1,12 @@
+﻿--Christopher Barnett
+--Lab 5
+
+--Query 1
+select a.city
+from agents a
+inner join orders o
+	on a.aid = o.aid
+inner join customers c
+	on c.cid = o.cid and c.name = 'Basics'
+order by city asc;
+
